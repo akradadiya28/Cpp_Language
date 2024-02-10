f2ea8dfb2d48ee7b2492b376f13ce3a7b25b6479
akradadiya28 <akradadiya28@gmail.com>
2024-02-10 11:46:07 +0530

    new files

---
+++ firstProgram.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout << "Jay Shree Ram" << endl;
+    cout << "Jay Shree Krishna" << endl;
+
+    return 0;
+}
---
+++ inputProgram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define MAX 555
+using namespace std;
+int main()
+{
+
+    int a, b, c;
+
+    cout << "Enter Value of a =";
+    cin >> a;
+
+    cout << "Enter Value of b =";
+    cin >> b;
+
+    c = a + b;
+
+    cout << "sum is = " << c << endl;
+
+    return 0;
+}
