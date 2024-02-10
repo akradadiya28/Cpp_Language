4218967c34b0b9d560841d2937a4f9bc841e4d53
akradadiya28 <akradadiya28@gmail.com>
2024-02-10 14:16:30 +0530

    delet some program

--- p3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main() {
-	int birthYear;
-	std::cout << "Please enter your birth year:";
-	std::cin >> birthYear;
-	int age = 2018 - birthYear;
-	std::cout << "Your age is: " << age;
-	std::cout << "Your class is Th-4" ;
-}
