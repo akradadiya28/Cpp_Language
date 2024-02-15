c1f121e47774d69f9b8610fff36a1ad582ff119f
akradadiya28 <akradadiya28@gmail.com>
2024-02-15 10:02:26 +0530

    const p add

--- const.cpp
+++ const.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
+#define A 5 // macro
 using namespace std;
 
 int main()
 {
-    // const int a = 5;
-#define a 5
+    // const int a = 5; //constant
 
     cout << "value of a is " << a << endl;
 
